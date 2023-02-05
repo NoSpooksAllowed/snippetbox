2ed1bb4eb70eca96cd0691d4c5bbfb026dc93daa
spook <hersgori4@protonmail.com>
2023-02-04 23:01:30 -0300

    add reading flags in main

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,11 +1,20 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 )
 
 func main() {
+	addr := flag.String("addr", ":4000", "HTTP network address")
+
+	// Importantly, we use te flag.Parse() function to parse the command line
+	// This reads in the command-line flag value and assigns it to the addr
+	// variable. You need to call this *before* you use the addr variable
+	// otherwise it will always contain the default value of ":4000"
+	// If any error encountered during parsing te application will be terminated
+	flag.Parse()
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet", showSnippet)
@@ -15,7 +24,10 @@ func main() {
 
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-	log.Println("Starting server on :4000")
-	err := http.ListenAndServe(":4000", mux)
+	// The value returned from te flag.String() functions is a pointer the flag
+	// value, not the value itself. So we need to dereference the pointer
+	// before using it
+	log.Printf("Starting server on %s", *addr)
+	err := http.ListenAndServe(*addr, mux)
 	log.Fatal(err)
 }
