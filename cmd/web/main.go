ad170c72be7ad6e4a797936fc71e7cee7137d085
spook-work <hersgori4@protonmail.com>
2023-02-13 17:31:55 -0300

    add godotenv use to connect to MySQL pool

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
 )
 
 // Define an application struct to hold the application-wide dependencies for that
@@ -16,10 +21,20 @@ type application struct {
 }
 
 func main() {
+	envs, err := godotenv.Read(".env")
+
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
 	// Define a new command line flag with the name "addr", a default value of ":4000"
 	// and some short help that explaining what the flag controls. The value of
 	// flag will be stored in the addr variable at runtime
 	addr := flag.String("addr", ":4000", "HTTP network address")
+
+	// Define a new command-line flag for the MySQL DSN string.
+	authStr := fmt.Sprintf("%s:%s@/snippetbox?parseTime=true", envs["USERNAME"], envs["PASSWORD"])
+	dsn := flag.String("dsn", authStr, "MySQL database")
 
 	// Importantly, we use te flag.Parse() function to parse the command line
 	// This reads in the command-line flag value and assigns it to the addr
@@ -39,6 +54,18 @@ func main() {
 	// the desitnation and se the log.Lshortfile flag to include the relevante
 	// file name and line number.
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	// To keep the main() function tidy I've put the code for creating a connection
+	// pool into the separate openDB() function below. We pass openDB() the DSN
+	// from the command-line flag
+	db, err := openDB(*dsn)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+
+	// We also defer a call to db.Close(), so that the connection pool is close
+	// before the main() function exits.
+	defer db.Close()
 
 	// Initialize a new instance of application containing the dependencies.
 	app := &application{
@@ -60,6 +87,21 @@ func main() {
 	infoLog.Printf("Starting server on %s", *addr)
 
 	// Call the ListenAndServe() method on our new http.Server struct
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	errorLog.Fatal(err)
+}
+
+// The openDB() function wraps sql.Open() and returns a sql.DB connection pool
+// for a given DSN.
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }
