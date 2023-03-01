b123653bc102e1a2a2b4eca8684581fb54769a6d
spook-work <hersgori4@protonmail.com>
2023-03-01 19:35:05 -0300

    created new middleware chain contains only specific to our dynamic application routes and update these routes to the new middleware chain

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -12,14 +12,22 @@ func (app *application) routes() http.Handler {
 	// which will be used for every request our application receives.
 	standardMiddleware := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
 
+	// Create a new middleware chain containing the middleware specific to our
+	// dynamic application routes. For now, this chain will only contain
+	// the session middleware but we'll add more to it later.
+	dynamicMiddleware := alice.New(app.session.Enable)
+
 	// Swap the route declarations to use the applcation struct's methods as the
 	// handler functions.
 	mux := pat.New()
-	mux.Get("/", http.HandlerFunc(app.home))
-	mux.Get("/snippet", http.HandlerFunc(app.createSnippetForm))
-	mux.Get("/snippet/create", http.HandlerFunc(app.createSnippetForm))
-	mux.Post("/snippet/create", http.HandlerFunc(app.createSnippet))
-	mux.Get("/snippet/:id", http.HandlerFunc(app.showSnippet))
+
+	// Update these routes to use the new dynamicMiddleware chain followed
+	// by the appropriate handler function.
+	mux.Get("/", dynamicMiddleware.ThenFunc(app.home))
+	mux.Get("/snippet", dynamicMiddleware.ThenFunc(app.createSnippetForm))
+	mux.Get("/snippet/create", dynamicMiddleware.ThenFunc(app.createSnippetForm))
+	mux.Post("/snippet/create", dynamicMiddleware.ThenFunc(app.createSnippet))
+	mux.Get("/snippet/:id", dynamicMiddleware.ThenFunc(app.showSnippet))
 
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 
