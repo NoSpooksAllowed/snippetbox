17285fdacaa489ce88151a2818eb9d03c93d1090
spook-work <hersgori4@protonmail.com>
2023-03-06 15:36:51 -0300

    removed unexisted field from table users in sql query

--- pkg/models/mysql/users.go
+++ pkg/models/mysql/users.go
@@ -51,7 +51,7 @@ func (m *UserModel) Authenticate(email, password string) (int, error) {
 	var id int
 	var hashedPassword []byte
 
-	stmt := `SELECT id, hashed_password FROM users WHERE email = ? AND active = TRUE`
+	stmt := `SELECT id, hashed_password FROM users WHERE email = ?`
 	row := m.DB.QueryRow(stmt, email)
 	err := row.Scan(&id, &hashedPassword)
 
