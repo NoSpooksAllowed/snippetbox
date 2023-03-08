670a1910376d2550ce80bb2b24472ae2bc41019d
spook-work <hersgori4@protonmail.com>
2023-03-08 16:47:16 -0300

    now auhenticatedUser method returns *models.User variable or nil

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -7,6 +7,7 @@ import (
 	"runtime/debug"
 	"time"
 
+	"github.com/NoSpooksAllowed/snippetbox/pkg/models"
 	"github.com/justinas/nosurf"
 )
 
@@ -79,6 +80,11 @@ func (app *application) addDefaultData(td *templateData, r *http.Request) *templ
 
 // The aunthenticatedUser method returns the ID of the current user from the
 // session, or zero if the request is from an unauthenticated user.
-func (app *application) aunthenticatedUser(r *http.Request) int {
-	return app.session.GetInt(r, "userID")
+func (app *application) aunthenticatedUser(r *http.Request) *models.User {
+	user, ok := r.Context().Value(contextKeyUser).(*models.User)
+	if !ok {
+		return nil
+	}
+
+	return user
 }
