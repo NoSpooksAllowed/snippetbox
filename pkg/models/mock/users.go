da1a8bd31417f3bdb6a4be08d0f48ebe595c0f5c
spook-work <hersgori4@protonmail.com>
2023-03-11 18:51:04 -0300

    finished mock users struct and methods

--- pkg/models/mock/users.go
+++ pkg/models/mock/users.go
@@ -17,6 +17,27 @@ type UserModel struct{}
 
 func (m *UserModel) Insert(name, email, passwrod string) error {
 	switch email {
-		case "dupe@example"
+	case "dupe@example":
+		return models.ErrDuplicateEmail
+	default:
+		return nil
+	}
+}
+
+func (m *UserModel) Authenticate(email, password string) (int, error) {
+	switch email {
+	case "alice@example.com":
+		return 1, nil
+	default:
+		return 0, models.ErrInvalidCredentials
+	}
+}
+
+func (m *UserModel) Get(id int) (*models.User, error) {
+	switch id {
+	case 1:
+		return mockUser, nil
+	default:
+		return nil, models.ErrNoRecord
 	}
 }
